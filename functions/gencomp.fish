84490531a9b024dc2244f571de4ea429d1c1635b
Ryota Kobayashi <rytakov@gmail.com>
2017-07-21 22:44:56 +0900

    Fix regex and the default action

--- functions/gencomp.fish
+++ functions/gencomp.fish
@@ -64,7 +64,7 @@ function gencomp -d 'generate completions for fish-shell with usage messages'
                 # e.g.)
                 # COMMANDS
                 #     command, c   do something
-                set -l words (string match -r -- '^ +([\w-]+)(?:, *)\w(?:[,= ] *)(.*)' "$line")
+                set -l words (string match -r -- '^([\w-]+)(?:, *)\w(?:[,= ] *)(.*)' "$line")
                 if test (count $words) = 3
                     __gencomp_subcommand_completion "$cmd" "$words[2]" "$words[3]"
                     if test "$parse_subcommand" = true
@@ -76,7 +76,7 @@ function gencomp -d 'generate completions for fish-shell with usage messages'
                 # e.g.)
                 # COMMANDS
                 #     command    do simething
-                set -l words (string match -r -- '^ +(\w+)(?:[,= ] *)(.*)' "$line")
+                set -l words (string match -r -- '^(\w+)(?:[,= ] *)(.*)' "$line")
                 if test (count $words) = 3
                     __gencomp_subcommand_completion "$cmd" "$words[2]" "$words[3]"
                     if test "$parse_subcommand" = true
@@ -165,7 +165,7 @@ function gencomp -d 'generate completions for fish-shell with usage messages'
         if count $commands >/dev/null
             set action complete
         else
-            set list
+            set action list
         end
     end
     
